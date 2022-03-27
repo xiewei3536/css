f3f4e66a86c89a8defda2fca0378c50f6a0f9fc1
xiewei3536 <willy880305@gmail.com>
2022-03-27 10:47:15 +0800

    first

---
+++ alistSkin_2.2/js/alistSkin.js
@@ -0,0 +1,54 @@
+/*
+ * name : Future
+ * edition : 2.0.2
+ * message : 前端美化样式由星环[Future]提供，程序由Alist创始者友情提供
+ * technical support : https://www.xhto.cn/
+ */
+
+var x = true;
+
+$("html").click(function() {
+    if (x) {
+        var title = document.title,
+            // 个人信息编写区域+星环博客提供技术支持+ —————— ON
+            歡迎蒞臨agu文件專業代下工作室
+            // 头像链接地址
+            headimg = "",
+            // 博主铭言句子
+            motto = "",
+            // 配置 - 导航内容及链接【注：名称和链接需要一一对应，用英文逗号隔开】
+            linkname = ["首頁", "Agu專業文件代下"],
+            link = ["/", "https://agu.xbwsh.com/"],
+            linkname = ["蝦皮店", "Agu文件代下工作室"],
+            link = ["/", "https://shopee.tw/puccaaigaru"]
+            code = "";
+
+        // 个人信息编写区域+星环博客提供技术支持+ —————— END
+
+        // 程序化代码
+        code += '<h3>' + title + '</h3>';
+        code += '<div >< img src = "' + headimg + '" alt = "[ ' + title + ' ]" / >< /div>';
+        code += '<p>' + motto + '</p >';
+        code += '< ul >';
+        for (vo in linkname) {
+            code += '<li> < a href = "' + link[vo] + '" > ' + linkname[vo] + ' < /a></li >';
+        }
+        code += '< /ul>';
+
+        // 代码写入视觉程序
+        $(".header").append(code);
+    }
+})
+
+// 支持样式代码
+$(document).on('click', '.buttons>span:eq(1) svg', function() {
+    var $viwe = $(this).attr('viewBox');
+    alert($viwe);
+})
+
+window.onload = function() {
+    setTimeout(function() {
+        $("html").click();
+        x = false;
+    }, 100);
+}
